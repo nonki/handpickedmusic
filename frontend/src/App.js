2d1fca4072aeffeea57ddece8d93976db681f868
Thomas Flack <thomas.flack@skybettingandgaming.com>
2021-10-03 21:52:05 +0100

    Add basic welcome page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { createTheme, responsiveFontSizes, ThemeProvider } from '@mui/material/styles';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import { createContext, useState, useMemo } from 'react';
+
+export const ColorModeContext = createContext({ toggleColorMode: () => {} });
 
 function App() {
   return (
+    <Container>
+      <Typography variant="h2">
+        Welcome!
+      </Typography>
+    </Container>
   );
 }
 
-export default App;
+const ToggleColorMode = () => {
+  const [primaryColor, setPrimaryColor] = useState('#d796e9');
+  const colorMode = useMemo(
+    () => ({
+      setPrimaryColor: (primaryColor) => {
+        setPrimaryColor(primaryColor);
+      }
+    }),
+    []
+  );
+
+  const theme = useMemo(
+    () =>
+      responsiveFontSizes(createTheme({
+        palette: {
+          mode: 'light',
+          primary: {
+            main: primaryColor,
+          },
+        },
+      })),
+    [primaryColor]
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default ToggleColorMode;
