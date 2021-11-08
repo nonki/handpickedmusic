8fc65b8eb9906923aa4babd76621df09acb83552
Thomas Flack <thomas.flack@skybettingandgaming.com>
2021-11-08 00:30:39 +0000

    working lambda resolver

--- amplify/backend/function/RandomTrack/src/go.mod
+++ amplify/backend/function/RandomTrack/src/go.mod
@@ -2,4 +2,12 @@ module lambda
 
 go 1.17
 
-require github.com/aws/aws-lambda-go v1.27.0
+require (
+	github.com/aws/aws-lambda-go v1.27.0
+	github.com/machinebox/graphql v0.2.2
+)
+
+require (
+	github.com/matryer/is v1.4.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
--- amplify/backend/function/RandomTrack/src/main.go
+++ amplify/backend/function/RandomTrack/src/main.go
@@ -2,14 +2,42 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	graphql "github.com/machinebox/graphql"
 )
 
-func HandleRequest(ctx context.Context) (string, error) {
-	return fmt.Sprintf("API_URL: %s\nAPI_KEY: %s", os.Getenv("API_URL"), os.Getenv("API_handndpickedmusic_GRAPHQLAPIKEYOUTPUT")), nil
+type Track struct {
+	ID string `json:"id"`
+}
+
+func HandleRequest(ctx context.Context) ([]Track, error) {
+	client := graphql.NewClient(os.Getenv("API_HANDPICKEDMUSIC_GRAPHQLAPIENDPOINTOUTPUT"))
+
+	req := graphql.NewRequest(`
+	query myQuery {
+		listTracks {
+			items {
+				id
+			}
+		}
+	}
+	`)
+
+	var respData struct {
+		ListTracks struct {
+			Items []Track `json:"items"`
+		} `json:"listTracks"`
+	}
+
+	req.Header.Set("X-API-KEY", os.Getenv("API_HANDPICKEDMUSIC_GRAPHQLAPIKEYOUTPUT"))
+
+	if err := client.Run(ctx, req, &respData); err != nil {
+		return []Track{}, err
+	}
+
+	return respData.ListTracks.Items, nil
 }
 
 func main() {
