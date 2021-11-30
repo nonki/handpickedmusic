7e3e8a124f410fc7f4c95e679a39bef2b2a2146c
Thomas Flack <thomas.flack@skybettingandgaming.com>
2021-11-30 15:45:08 +0000

    main: Add date for uploading

--- src/Upload.js
+++ src/Upload.js
@@ -1,7 +1,12 @@
 import { useState, useContext } from 'react';
-import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
 import Input from '@mui/material/Input';
+import TextField from '@mui/material/TextField';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DesktopDatePicker from '@mui/lab/DesktopDatePicker';
 import Typography from '@mui/material/Typography';
 import { TrackContext } from './App.js';
 
@@ -13,12 +18,14 @@ const Upload = () => {
   const context = useContext(TrackContext)
   const [url, setUrl] = useState()
   const [error, setError] = useState("")
+  const [date, setDate] = useState(new Date())
+  const [schedule, setSchedule] = useState(false)
   const getId = (url) => {
     const parsedUrl = new URL(url)
     return parsedUrl.pathname.split("/").pop()
   }
 
-  async function uploadTrack(id) {
+  async function uploadTrack(id, date) {
     const tracksList = await API.graphql(graphqlOperation(listTracks, { filter: {spotifyId: {eq: id}} }))
     const items = tracksList.data.listTracks.items
     if (items.length > 0) {
@@ -26,7 +33,17 @@ const Upload = () => {
       return
     }
 
-    API.graphql(graphqlOperation(createTrack, {input: {spotifyId: id}}))
+    if (date !== null) {
+      const scheduledList = await API.graphql(graphqlOperation(listTracks, { filter: {date: {eq: date}} }))
+      const scheduledItems = scheduledList.data.listTracks.items
+      if (scheduledItems.length > 0) {
+        setError("Entry already exists for that date " + date)
+        return
+      }
+    }
+
+    console.log(`Uploading ${id} for date ${date}`)
+    API.graphql(graphqlOperation(createTrack, {input: {spotifyId: id, date: date}}))
     setError("")
   }
 
@@ -36,7 +53,7 @@ const Upload = () => {
   }
 
   const submit = () => {
-    uploadTrack(getId(url))
+    uploadTrack(getId(url), schedule ? date.toISOString().split('T')[0] : null)
     context.setPreview(false)
     context.setTrackId("")
     setUrl("")
@@ -49,14 +66,36 @@ const Upload = () => {
   const errorEl = <Typography variant='error'>{error}</Typography>
 
   return (
-    <Box>
+    <Stack
+      sx={{
+        pt: 4,
+      }}
+      justifyContent="center"
+      direction="row"
+      spacing={2}
+    >
       <Input value={url} onChange={(e) => setUrl(e.target.value)} />
+      <Checkbox
+        color="secondary"
+        checked={schedule}
+        onChange={() => setSchedule(!schedule)}
+      />
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <DesktopDatePicker
+            label="Date"
+            disabled={!schedule}
+            inputFormat="MM/dd/yyyy"
+            value={date}
+            onChange={(d) => setDate(d)}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </LocalizationProvider>
       <Button variant='contained' color='secondary' onClick={() => preview()}>
         Preview
       </Button>
       { context.preview && submitButton }
       { error !== "" && errorEl }
-    </Box>
+    </Stack>
   )
 }
 
